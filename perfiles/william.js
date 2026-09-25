7384a415102199ee4a1afb65c0f731552a6be62d
William <willifhernandez@gmail.com>
2026-09-25 13:38:00 +0200

    add: crea la tarjeta de perfil de William

---
+++ perfiles/william.js
@@ -0,0 +1,11 @@
+// 👇 Copia este archivo como perfiles/tunombre.js y rellena TUS datos.
+// Después, añade "tunombre" a perfiles/lista.js para que aparezca tu tarjeta.
+
+registrarPerfil({
+  nombre: "William Hern´ández",
+  emoji: "🧛‍♂️",
+  rol: "Estudiante de Bootcamp",
+  lenguajeFavorito: "ReactJS",
+  datoCurioso: "Me gusta el Voley",
+  github: "wfhgdev",
+});
