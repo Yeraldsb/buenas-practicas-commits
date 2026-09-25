3c72f8113d5887d8434bcf6abce407a1bdcaddbe
Marisa-Ruiz <marisa120685@gmail.com>
2026-09-25 13:14:11 +0200

    add: create Marisa’s profile card

---
+++ perfiles/marisa.js
@@ -0,0 +1,11 @@
+// 👇 Copia este archivo como perfiles/tunombre.js y rellena TUS datos.
+// Después, añade "tunombre" a perfiles/lista.js para que aparezca tu tarjeta.
+
+registrarPerfil({
+  nombre: "Marisa Ruiz",
+  emoji: "💻",
+  rol: "Desarrolladora web junior",
+  lenguajeFavorito: "CSS / React",
+  datoCurioso: "Devorador de series a ritmo industrial, creador de foros experimentales y fiel creyente de que el dulce ayuda a resolver cualquier bug.",
+  github: "Marisa-Ruiz",
+});
