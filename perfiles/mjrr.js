8f5d4a8ca4204a3d326827e38b7647b38dc4ff10
María José <mjrr39sevilla@gmail.com>
2026-09-25 12:56:39 +0200

    add: create personal profile file and update member list

---
+++ perfiles/mjrr.js
@@ -0,0 +1,11 @@
+// 👇 Copia este archivo como perfiles/tunombre.js y rellena TUS datos.
+// Después, añade "tunombre" a perfiles/lista.js para que aparezca tu tarjeta.
+
+registrarPerfil({
+  nombre: "mjrr",
+  emoji: "🧙‍♂️",
+  rol: "Developer",
+  lenguajeFavorito: "Python (sí, cuenta como lenguaje 😄)",
+  datoCurioso: "Me gusta leer libros",
+  github: "https://github.com/mjrr39sevilla",
+});
