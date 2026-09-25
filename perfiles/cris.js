3eae87a41a8a6041804bddc13ad7a220d5b03399
cristinarodriguezl-dev <cristinarodriguez.cv@gmail.com>
2026-09-25 12:22:43 +0200

    add: crea la tarjeta de perfil de Cris

---
+++ perfiles/cris.js
@@ -0,0 +1,8 @@
+registrarPerfil({
+  nombre: "Cris",
+  emoji: "🐈‍⬛",
+  rol: "Developer",
+  lenguajeFavorito: "Python",
+  datoCurioso: "Las galaxias más lejanas se alejan de nosotros más rápido que la velocidad de la luz.",
+  github: "cristinarodriguez-dev",
+});
--- perfiles/lista.js
+++ perfiles/lista.js
@@ -4,4 +4,5 @@
 
 const LISTA_PERFILES = [
   "profe",
+  "cris"
 ];
